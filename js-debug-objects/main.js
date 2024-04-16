284f54e71a0f0f7b18c259a8671d8bbea8e2a22d
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-16 16:21:45 +0200

    feat: debug objects

--- js-debug-objects/main.js
+++ js-debug-objects/main.js
@@ -56,7 +56,7 @@ const cars = [
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
@@ -64,14 +64,14 @@ const cars = [
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
 
-const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
-});
 
-const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+const gasolineCars = cars.filter((auto) => auto.type.toLowerCase() === 'benzina');
+
+const dieselCars = cars.filter((auto) => auto.type.toLowerCase() === 'diesel');
+
+const otherCars = cars.filter((auto) => {
+    return auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel';
 });
 
 console.log('Auto a benzina');
@@ -84,3 +84,11 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+
+// 1 - manca una virgola negli object
+// 2 - arrow function scritta al contrario in gasoline cars
+// 3 - parametro cars nell'arrow function non va bene perché poi utilizziamo auto, e giustamente non è definita perché lo legge come una variabile non come il valore dell'array ad ogni ciclo.
+// 4 - nell'array di oggetti il valore dell type è scritto a volte con delle maiuscole e minuscole, per questo non prende tutte le benzina, perché non è scritto perfettamente uguale, devo convertire tutto in minuscolo prima di confrontarlo, questo vale anche negli altri filter.
+// 5 - nel filtro per le auto diesel, essendo nell'arrow function che ritorna direttamente una sola espressione ovver fa solo un confronto allora le graffe si possono omettere
+// 6 - invece di booliano or va usato and perché ora basta che solo una della due condizioni sia true allora passa con and devono essere true entrambe
